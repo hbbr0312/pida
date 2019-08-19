dee145d4aec895daf18e84dcd11985344efa7ea4
guseul <hbbr@kaist.ac.kr>
2019-08-19 13:23:58 +0900

    commit

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React from "react";
-import MainNavigation from "./navigation/MainNavigation";
-import { ApolloProvider } from "react-apollo";
-import defaultClient from "./Apollo/client";
-import Login from "./screens/Login";
-import { login } from "./api";
+import React from "react"
+import MainNavigation from "./navigation/MainNavigation"
+import { ApolloProvider } from "react-apollo"
+import defaultClient from "./Apollo/client"
+import Login from "./screens/Login"
+import { login } from "./api"
 
 export default function App() {
-  const logined = true;
+  const logined = true
+  //login({ username: "final", password: "1234" })
   if (logined)
     return (
       <ApolloProvider client={defaultClient}>
         <MainNavigation />
       </ApolloProvider>
-    );
-  else return <Login />;
+    )
+  else return <Login />
 }
--- api.js
+++ api.js
@@ -1,29 +1,27 @@
-import axios from "axios";
-
 const BASE_URL =
-  "http://ec2-13-125-246-38.ap-northeast-2.compute.amazonaws.com/";
+  "http://ec2-13-125-246-38.ap-northeast-2.compute.amazonaws.com/"
 
 export const getCategory = () => {
-  const url = BASE_URL + "categories";
+  const url = BASE_URL + "categories"
   return fetch(url)
     .then(res => res.json())
-    .catch(error => console.log(error));
-};
+    .catch(error => console.log(error))
+}
 
 export const getProduct = url => {
   return fetch(url)
     .then(res => res.json())
-    .catch(error => console.log(error));
-};
+    .catch(error => console.log(error))
+}
 
 export const getReview = url => {
   return fetch(url)
     .then(res => res.json())
-    .catch(error => console.log(error));
-};
+    .catch(error => console.log(error))
+}
 
 export const register = async info => {
-  let response;
+  let response
   try {
     response = await fetch(BASE_URL + "users/", {
       method: "POST",
@@ -40,33 +38,53 @@ export const register = async info => {
         Accept: "application/json",
         "Content-Type": "application/json"
       }
-    });
+    })
   } catch (err) {
-    console.log("error:", err);
+    console.log("error:", err)
   } finally {
-    console.log("/response/", response);
-    return response;
+    console.log("/response/", response)
+    return response
   }
-};
+}
+
+export const checkDuplicateId = async email => {
+  const response = await fetch(BASE_URL + "users/" + email, {
+    method: "GET"
+  })
+  if (response.status === 401) return false
+  else if (response.status === 404) return true
+}
 
 export const login = async info => {
-  let response;
+  let response
+  const client_id = "yg30yWvkbNXIjDbA4mDLimNkyCgZpriBy6c5k8yU"
+  const grant_type = "password"
+  const client_secret =
+    "4yRNL6m1LUsHPP8ohiDLfnnPVQ8Vikh1EMGYSRhsTKzDRZoAKYZm2HZPe4Ls9HTJuTwjJddcFJmivKCVtAve5yPzmJ9M6pO5XGmh3DmARscXu4L8cRSrk8XpkoXHYFZW"
+  const strParams =
+    "username=" +
+    info.username +
+    "&password=" +
+    info.password +
+    "&grant_type=" +
+    grant_type +
+    "&client_id=" +
+    client_id +
+    "&client_secret=" +
+    client_secret
   try {
-    response = await fetch(BASE_URL + "o/token/", {
+    response = await fetch(BASE_URL + "o/token/" + "?" + strParams, {
       method: "POST",
-      parameters: JSON.stringify({
-        username: info.username,
-        password: info.password
-      }),
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/json"
       }
-    });
+    })
   } catch (err) {
-    console.log("error:", err);
+    console.log("error:", err)
   } finally {
-    console.log("/response/", response);
-    return response;
+    console.log("/response/", response)
+    console.log("/access_token/", response.access_token)
+    console.log("/refresh_token/", response.refresh_token)
+    return response
   }
-};
+}
